7991bc2c9cec9b5d60a27ed00ed9d2399fcb0baf
Bernardo B Rezende <bernardobrezende@gmail.com>
2011-06-29 00:15:05 -0300

    Adding Windows Application grid visualization.

--- src/dengueSim.App/World.Designer.cs
+++ src/dengueSim.App/World.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace dengueSim.App
+namespace dengueSim.App
 {
     partial class World
     {
@@ -29,11 +29,52 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.CausesValidation = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(973, 640);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // World
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(973, 640);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "World";
+            this.Text = "WoD - World of Dengue";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- src/dengueSim.App/World.cs
+++ src/dengueSim.App/World.cs
@@ -1,19 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using dengueSim.Domain;
 
 namespace dengueSim.App
 {
     public partial class World : Form
     {
+        private Ambiente amb;
+        private Random rd = new Random();
+
         public World()
         {
             InitializeComponent();
+            amb = new Ambiente(App.Default.TamanhoMundo, 0, 0, 0, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            for (int i = 0; i < amb.Tamanho; i++)
+            {
+                dataGridView1.Columns.Add(i.ToString(), i.ToString());
+                dataGridView1.Columns[i].Width = 30;
+            }
+
+            dataGridView1.Rows.Add(amb.Tamanho);
+            Draw();
+        }
+
+        public void Draw()
+        {
+            string agentesNoLocal = String.Empty;
+            for (int i = 0; i < amb.Tamanho; i++)
+            {
+                for (int j = 0; j < amb.Tamanho; j++)
+                {
+                    dataGridView1[j, i].Value = null;
+                    dataGridView1[j, i].Style.BackColor = Color.White;
+                    //Pinta os focos
+                    if (amb[i, j] is Foco)
+                    {
+                        dataGridView1[j, i].Style.BackColor = Color.Yellow;
+                    }
+                    foreach (Agente ag in amb[i, j].Agentes)
+                    {
+                        agentesNoLocal += ag.Draw();
+                        if (ag is Pessoa)
+                        {
+                            dataGridView1[j, i].Style.ForeColor = Color.Black;
+                        }
+
+                        if (ag is AgenteDeSaude)
+                        {
+                            dataGridView1[j, i].Style.BackColor = Color.Brown;
+                            dataGridView1[j, i].Style.ForeColor = Color.White;
+                        }
+                    }
+                    dataGridView1[j, i].Value = agentesNoLocal;
+                    agentesNoLocal = String.Empty;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            amb.Executar();
+            amb.Atualizar();
+            this.Draw();
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                timer1.Enabled = !timer1.Enabled;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if (timer1.Enabled)
+                {
+                    timer1.Enabled = false;
+                }
+                timer1_Tick(this, EventArgs.Empty);
+            }
         }
     }
 }
